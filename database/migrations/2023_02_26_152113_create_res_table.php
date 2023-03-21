89daf2e88455a1f6433f91b924cbf4cccee5e0fa
iftesum akter <iftesumakter@gmail.com>
2023-03-21 17:05:37 +0600

    hello

---
+++ database/migrations/2023_02_26_152113_create_res_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('res', function (Blueprint $table) {
+            $table->id();
+            $table->string('Writer_Name');
+            $table->string('book_name');
+            $table->string('quantity');
+            
+            $table->string('publication');
+           
+            $table->integer('price');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('res');
+    }
+}
